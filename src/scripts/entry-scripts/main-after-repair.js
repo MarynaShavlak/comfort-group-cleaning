181e23fb71e5f30bb33e91d8ab529d129ca62bb2
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-10-29 19:56:57 +0200

    add docs

--- src/scripts/entry-scripts/main-after-repair.js
+++ src/scripts/entry-scripts/main-after-repair.js
@@ -3,6 +3,7 @@ import '../modules/aside-menu.js';
 import '../modules/theme-toggler.js';
 import '../modules/scroll-to-top.js';
 import '../modules/questions.js';
+import '../modules/reviews.js';
 import '../modules/calendar.js';
 import '../modules/time-picker.js';
 import '../modules/location.js';
