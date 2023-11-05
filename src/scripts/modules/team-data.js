8c5f160eb0b6bd15ed8410844ece1a17987322c6
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-11-06 01:26:13 +0200

    add slider for cleaners category

---
+++ src/scripts/modules/team-data.js
@@ -0,0 +1,65 @@
+export const teamMembersData = [
+    {
+      name: "Іван Стефанюк",
+      memberID: 'member-1',
+      experience: '3 роки',
+      quote: "Прибирання – це не просто робота, це спосіб зробити світ кращим",
+      category: 'window-cleaner',
+  },
+  {
+    name: "Наталя Ковальська",
+    memberID: 'member-2',
+    experience: '5 років',
+    quote: "Чистий дім - щасливий дім",
+    category: 'window-cleaner',
+},
+  {
+      name: "Олена Новак",
+      experience: '7 років',
+      memberID: 'member-3',
+      quote: "У чистому просторі можна знайти спокій і ясність" ,
+      category: 'window-cleaner',
+  },
+  {
+    name: "Катерина Войчик",
+    experience: '2 роки',
+    memberID: 'member-4',
+    quote: "Чистий дім допомагає активізувати продуктивні думки",
+    category: 'window-cleaner',
+},
+  {
+      name: "Роман Пятаковський",
+      experience: '4 роки',
+      memberID: 'member-5',
+      quote: "Чистота є найкращим відображенням добре організованого розуму",
+      category: 'window-cleaner',
+  },
+  {
+    name: "Міхал Горбач",
+    experience: '4 роки',
+    memberID: 'member-6',
+    quote: "Чистота поруч із благочестям",
+    category: 'window-cleaner',
+},
+    {
+      name: "Joanna Ivanenko",
+      experience: '6 років',
+      quote: "Cleaning with care creates a welcoming atmosphere"
+  },
+  
+  {
+      name: "Agnieszka Lysenko",
+      experience: '8 років',
+      quote: "Dust-free homes, happy hearts"
+  },
+  {
+      name: "Tomasz Dubrowski",
+      experience: '5 років',
+      quote: "Clean today for a brighter tomorrow"
+  },
+  {
+      name: "Ania Kovalenko",
+      experience: '6 років',
+      quote: "A sparkling space brings a smile to every face"
+  }
+];
--- src/scripts/modules/team-slider.js
+++ src/scripts/modules/team-slider.js
@@ -1,48 +1,101 @@
-const slider = document.querySelector('.team__circular-slider');
-const prevButton = document.querySelector('.team__btn-prev');
-const nextButton = document.querySelector('.team__btn-next');
-const slideClasses = ['slide-1', 'slide-2', 'slide-3', 'slide-4', 'slide-5', 'slide-6'];
-let currentIndex = 0;
-nextButton.addEventListener('click', () => changeSlide(1));
-prevButton.addEventListener('click', () => changeSlide(-1));
+import { teamMembersData } from "./team-data";
 
-function changeSlide(direction) {
-  currentIndex = (currentIndex + direction + slideClasses.length) % slideClasses.length;
-  updateSlider(direction);
+
+function createCircularSlider(config) {
+    // const { sliderSelector, prevButtonSelector, nextButtonSelector, slideClasses } = config;
+    const { sliderSelector, prevButtonSelector, nextButtonSelector, slideClasses, category } = config;
+    let currentIndex = 0;
+    const slides = slideClasses.map(className => document.querySelector(`.team__circular-section.${className}`));
+    
+
+    updateMemberTablo(slides, currentIndex + 1);
+    const slider = document.querySelector(sliderSelector);
+    const prevButton = document.querySelector(prevButtonSelector);
+    const nextButton = document.querySelector(nextButtonSelector);
+  
+    prevButton.addEventListener('click', () => changeSlide(-1));
+    nextButton.addEventListener('click', () => changeSlide(1));
+    slider.addEventListener('click', handleSlideClick);
+
+function handleSlideClick(e) {
+    const clickedItem = e.target;
+    if (clickedItem.tagName !== 'IMG') return;
+    const slide = clickedItem.closest('li');
+    if (!slide || slide.classList.contains('team-active-slide')) return;
+    const slideClass = slide.classList[1];
+         if (slideClass === 'slide-3') {
+        changeSlide(-1);
+      } else if (slideClass === 'slide-1') {
+        changeSlide(1);
+      }
 }
 
 
+    function changeSlide(direction) {
+        currentIndex = (currentIndex + direction + slideClasses.length) % slideClasses.length;
+        updateSlider(direction);
+    }
+      
+    function updateSlider(direction) {
+          const slides = slideClasses.map(className => document.querySelector(`.team__circular-section.${className}`));
+          removeActiveClassFromSlides(slides);
+          const activeClassIndex = direction === 1 ? 0 : 2;
+          moveSlides(slides, direction );
+          addActiveClassToSlide(slides, activeClassIndex);
+          updateMemberTablo(slides, activeClassIndex);
+    }
+        
+    function moveSlides(slides, direction ) {
+          const slideCount = slideClasses.length;
+          if (direction === 1) {
+              slides.forEach((slide, index) => {
+                 const newIndex = (index + direction) % slideCount;
+                 slide.classList.replace(slideClasses[index], slideClasses[newIndex]);
+               });
+         } else if (direction === -1) {
+             slides.forEach((slide, index) => {
+                 const newIndex = (index + direction + slideCount) % slideCount;
+                 slide.classList.replace(slideClasses[index], slideClasses[newIndex]);
+             });
+         }
+    }
+      
+    function removeActiveClassFromSlides(slides) {
+          slides.forEach(slide => slide.classList.remove('team-active-slide'));
+    }
+      
+    function addActiveClassToSlide(slides,index) {
+          slides[index].classList.add('team-active-slide');
+    }
+    
+    function getActiveSlideData(slides, index ) {
+        const memberID = slides[index].getAttribute('data-member');
+        const memberData= teamMembersData.find(member => member.memberID === memberID);
+        return memberData;
+    }
+    function updateMemberTablo(slides, index) {
+        const titleEl = document.querySelector('.member-info__title');
+        const expEl = document.querySelector('.member-info__exp');
+        const quoteEl = document.querySelector('.member-info__quote');
+        const {name,  experience, quote} = getActiveSlideData(slides, index );
+        titleEl.innerHTML = name;
+        expEl.innerHTML = `Досвід роботи: ${experience}`;
+        quoteEl.innerHTML = `<i class="fa-solid fa-quote-left"></i> ${quote} <i class="fa-solid fa-quote-right"></i>`
 
 
-function updateSlider(direction) {
-    const slides = slideClasses.map(className => document.querySelector(`.team__circular-section.${className}`));
-    removeActiveClassFromSlides(slides);
-    const activeClassIndex = direction === 1 ? 0 : 2;
-    moveSlides(slides, direction );
-    addActiveClassToSlide(slides, activeClassIndex);
-  }
-  
 
- function moveSlides(slides, direction ) {
-    const slideCount = slideClasses.length;
-    if (direction === 1) {
-        slides.forEach((slide, index) => {
-           const newIndex = (index + direction) % slideCount;
-           slide.classList.replace(slideClasses[index], slideClasses[newIndex]);
-         });
-   } else if (direction === -1) {
-       slides.forEach((slide, index) => {
-           const newIndex = (index + direction + slideCount) % slideCount;
-           slide.classList.replace(slideClasses[index], slideClasses[newIndex]);
-       });
-   }
- }
+    }
+}
+
+createCircularSlider({
+    sliderSelector: '.team__circular-slider',
+    category: 'window-cleaner',
+    prevButtonSelector: '.team__btn-prev',
+    nextButtonSelector: '.team__btn-next',
+    slideClasses: ['slide-1', 'slide-2', 'slide-3', 'slide-4', 'slide-5', 'slide-6'],
+  });
 
 
-function removeActiveClassFromSlides(slides) {
-    slides.forEach(slide => slide.classList.remove('team-active-slide'));
-  }
 
-  function addActiveClassToSlide(slides,index) {
-    slides[index].classList.add('team-active-slide');
-  }
+
+
